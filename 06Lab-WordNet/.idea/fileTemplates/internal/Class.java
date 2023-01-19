11a2c454919cb1ae5667b463b62de1b04c2d3c13
MolinDeng <zjumorning@gmail.com>
2023-01-19 21:56:08 +0800

    rm unrelated files

--- 06Lab-WordNet/.idea/fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#parse("File Header.java")
-
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-
-public class ${NAME} {
-    public static void main(String[] args) {
-        
-    }
-}
