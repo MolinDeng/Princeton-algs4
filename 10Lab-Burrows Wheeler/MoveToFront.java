f460189d743307625aa3cacd63ebbf1f2f8735cd
MolinDeng <zjumorning@gmail.com>
2023-05-04 00:14:18 +0800

    lab10: all ac

--- 10Lab-Burrows Wheeler/MoveToFront.java
+++ 10Lab-Burrows Wheeler/MoveToFront.java
@@ -7,40 +7,44 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
-import java.util.ArrayList;
-
 public class MoveToFront {
     private static final int R = 256;
-    private static ArrayList<Character> alphabet; // char : index
 
-    private static void reset() {
-        alphabet = new ArrayList<>();
-        for (char i = 0; i < R; i++) alphabet.add(i);
-    }
-
-    private static void update(int i) {
-        char c = alphabet.remove(i);
-        alphabet.add(0, c);
+    private static char[] init() {
+        char[] a = new char[R];
+        for (char i = 0; i < R; i++) a[i] = i;
+        return a;
     }
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        reset();
+        char[] index2Char = init();
         while (!BinaryStdIn.isEmpty()) {
-            int i = alphabet.indexOf(BinaryStdIn.readChar(8));
-            BinaryStdOut.write(i, 8);
-            update(i);
+            char c = BinaryStdIn.readChar(8);
+
+            int idx = 0;
+            for (; idx < R; idx++)
+                if (index2Char[idx] == c) break;
+            for (int i = idx; i > 0; i--)
+                index2Char[i] = index2Char[i - 1];
+            index2Char[0] = c;
+
+            BinaryStdOut.write(idx, 8);
         }
         BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
-        reset();
+        char[] index2Char = init();
         while (!BinaryStdIn.isEmpty()) {
-            int i = BinaryStdIn.readChar();
-            BinaryStdOut.write(alphabet.get(i), 8);
-            update(i);
+            int idx = BinaryStdIn.readChar();
+            char c = index2Char[idx];
+            for (int i = idx; i > 0; i--)
+                index2Char[i] = index2Char[i - 1];
+            index2Char[0] = c;
+
+            BinaryStdOut.write(c, 8);
         }
         BinaryStdOut.close();
     }
